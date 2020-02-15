7206f482dc2e50dd712692fd96e2fceb28dc9f8e
Josh Faison <Joshfaiso@yahoo.com>
2020-02-15 10:37:16 -0500

    fixing directory paths

--- app.js
+++ app.js
@@ -1,96 +1,99 @@
-const Manger = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require ("inquirer")
+let Manager = require("./lib/Manager");
+let Engineer = require("./lib/Engineer");
+let Intern = require("./lib/Intern");
+let inquirer = require("inquirer");
+let path = require("path");
+let fs = require("fs");
+let render = require('./lib/htmlRenderer.js');
+let id = 1;
+let constructorArray = [];
 
-let id = 1
-let constructorArray = []
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
 let askQuestions = () => {
+    inquirer.prompt([{
+        type: "list",
+        message: "What is your role?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern"]
+    }])
+        .then((userInput) => {
+            if (userInput.role === "Manager") {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your name?",
+                        name: "name"
+                    },
 
-   inquirer.prompt([{
-       type: "list",
-       message: "What is your role?",
-       name: "role",
-       choices: ["Manager", "Engineer", "Intern"]
-   }])
-    .then((userInput) =>{
-        if(userInput.role === "Manager"){
-            inquirer.prompt([
-                {
-                type: "input",
-                message: "What is your name?",
-                name: "name"
-                },
+                    {
+                        type: "input",
+                        message: "What is your email",
+                        name: "email"
+                    },
 
-                {
-                type: "input",
-                message: "What is your email",
-                name: "email"
-                },
-
-                {
-                type: "input",
-                message: "What is your office number",
-                name: "officeNumber"
-            }])
-            .then((userInput) => {
-                let manager = new Manger(userInput.name, id++, userInput.email, userInput.officeNumber)
-                constructorArray.push(manager)
+                    {
+                        type: "input",
+                        message: "What is your office number",
+                        name: "officeNumber"
+                    }])
+                    .then((userInput) => {
+                        let manager = new Manager(userInput.name, id++, userInput.email, userInput.officeNumber)
+                        constructorArray.push(manager)
                         console.log(constructorArray)
-                     askQuestions() 
-            })
-        }else if(userInput.role === "Engineer") {
-            inquirer.prompt([{
-                type: "input",
-                message: "What is your Name?",
-                name: "employeeName"
-            },
-            {
-                type:"input",
-                message:"What is your email?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "What is your github name?",
-                name: "githubName"
+                        askQuestions()
+                    })
+            } else if (userInput.role === "Engineer") {
+                inquirer.prompt([{
+                    type: "input",
+                    message: "What is your Name?",
+                    name: "employeeName"
+                },
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "email"
+                },
+                {
+                    type: "input",
+                    message: "What is your github name?",
+                    name: "githubName"
+                }
+
+                ])
+                    .then((userInput) => {
+                        let engineer = new Engineer(userInput.name, id++, userInput.email, userInput.githubName)
+
+                        constructorArray.push(engineer)
+                        console.log(constructorArray)
+                        askQuestions()
+                    })
+            } else if (userInput.role === "Intern") {
+                inquirer.prompt([{
+                    type: "input",
+                    message: "What is your name",
+                    name: "name"
+                },
+                {
+                    type: "input",
+                    message: "What is the name of the school you attend?",
+                    name: "schoolName"
+                },
+                {
+                    type: "input",
+                    message: "What is your email",
+                    name: "email"
+                }]).then((userInput) => {
+                    let intern = new Intern(userInput.name, id++, userInput.email, userInput.schoolName)
+                    constructorArray.push(intern)
+                    console.log(constructorArray)
+                    //askQuestions()
+                    constructorArray.forEach(displayItems);
+                })
             }
-        
-            ])
-            .then((userInput) => {
-                let engineer = new Engineer(userInput.name, id++, userInput.email, userInput.githubName)
-        
-                constructorArray.push(engineer)
-                console.log(constructorArray)
-               askQuestions()
-            })
-        }else if(userInput.role === "Intern") {
-            inquirer.prompt([{
-                type: "input",
-                message: "What is your name",
-                name: "name"
-            },
-        {
-            type: "input",
-            message: "What is the name of the school you attend?",
-            name: "schoolName"
-        },
-        {
-            type: "input",
-            message: "What is your email",
-            name: "email"
-        }]).then((userInput) => {
-            let intern = new intern(userInput.name, id++, userInput.email, userInput.schoolName)
-            constructorArray.push(intern)
-            console.log(constructorArray)
-           askQuestions()
         })
-        }
-    })
-
-
-  
 }
+askQuestions();
 
-askQuestions()
+ module.exports = render;
